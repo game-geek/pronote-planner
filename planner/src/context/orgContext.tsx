a60ed6172a8ee1eca7ad7edf2828f1e383d94b3e
403 <hackingprogrammingaquascaping@gmail.com>
2023-11-10 10:48:20 +0000

    added env variables for planner

--- planner/src/context/orgContext.tsx
+++ planner/src/context/orgContext.tsx
@@ -203,7 +203,7 @@ function OrgContextProvider({children}: {children: any}) {
     }
 
     async function firebase_create_account(username: string) {
-      const response = await fetch("https://musical-yodel-4969gqvg54xcjpxg-8081.preview.app.github.dev/create_user", {method: 'post', headers: {
+      const response = await fetch(process.env.SERVER + "/create_user", {method: 'post', headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       }, body: JSON.stringify({username})})
--- planner/src/main.tsx
+++ planner/src/main.tsx
@@ -6,6 +6,7 @@ import './index.css'
 import m from './web_assembly/hello3.js'
 import { BrowserRouter } from 'react-router-dom'
 import OrgContextProvider from './context/orgContext.tsx'
+require('dotenv').config()
 m().then(function(Module: any) {
   // @ts-ignore
   const ccallArrays = (func, returnType, paramTypes=[], params, {heapIn="HEAPF32", heapOut="HEAPF32", returnArraySize=1}={}) => {
